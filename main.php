99e5d8e3318875c4cc6a31d5a5f4735fb5455267
isaralimat <154309668+isaralimat@users.noreply.github.com>
2023-12-19 13:32:49 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="main_style.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>eroyal tech systems</title> 
+</head> 
+<body bgcolor="pink"> 
+    <header> 
+        <nav class="navbar"> 
+            <div class="logo"> 
+                <a href="/"> 
+                    <img src="https://th.bing.com/th/id/OIP.bD_-HeNurkhNjTIxIJ9PMAHaFL?rs=1&pid=ImgDetMain" alt="gfg_logo">
+                    
+                </a>  
+            </div> 
+            <div class="hamburger-menu"> 
+                <span class="line"></span> 
+                <span class="line"></span> 
+                <span class="line"></span>  
+            </div> 
+            <div class="nav-menu hide"> 
+            <a href="main.php">HOME</a> &NonBreakingSpace;
+            <a href="about_us.html">ABOUT US</a> &NonBreakingSpace;
+            <a href="informations.html">INFORMATIONS</a> &NonBreakingSpace;
+            <a href="contact_us.html">CONTACT US</a> &NonBreakingSpace;
+            <a href="login.html">LOGIN</a> &NonBreakingSpace;
+            <a href="signup.html">SIGNUP</a> &NonBreakingSpace;
+            </div> 
+        </nav> 
+    
+    <div class="container-menu">
+        <div class="container-menu-child"><br>
+            <h1>Welcome to eRoyal Tech systems</h1><br>
+            <br>
+            <p class="style">eRTS Services offers you a challenging and exciting career path that helps you learn, enjoy and contribute. We firmly believe that the quality and commitment of our employees is a major reason for our growth and success.</p>
+            <br>
+            <h2>Products</h2>
+            <br>
+            <p>
+            eRoyal Tech Systems product helps to manage many key processes within the stipulated time. Their support and regular product updates are very valuable for logistics business.
+            </p>
+            <br>
+            <div class="new">
+        <li>
+            <ul>
+            <section>eRoyal FREIGHT</section>
+            <section>eRoyal SCM</section>
+            <section>eRoyal IMPEX</section>
+            <section>eRoyal ICON</section>
+            <section>Royal IGM-EGM</section>
+            </ul>
+        </li>
+        </div>
+        </div>
+        
+    </div>
+    </header> 
+    <footer>
+    <style>
+.fa {
+  padding: 30px;
+  font-size: 20px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.fa-pinterest {
+  background: #cb2027;
+  color: white;
+}
+
+.fa-snapchat-ghost {
+  background: #fffc00;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+
+.fa-skype {
+  background: #00aff0;
+  color: white;
+}
+
+.fa-android {
+  background: #a4c639;
+  color: white;
+}
+
+.fa-dribbble {
+  background: #ea4c89;
+  color: white;
+}
+
+.fa-vimeo {
+  background: #45bbff;
+  color: white;
+}
+
+.fa-tumblr {
+  background: #2c4762;
+  color: white;
+}
+
+.fa-vine {
+  background: #00b489;
+  color: white;
+}
+
+.fa-foursquare {
+  background: #45bbff;
+  color: white;
+}
+
+.fa-stumbleupon {
+  background: #eb4924;
+  color: white;
+}
+
+.fa-flickr {
+  background: #f40083;
+  color: white;
+}
+
+.fa-yahoo {
+  background: #430297;
+  color: white;
+}
+
+.fa-soundcloud {
+  background: #ff5500;
+  color: white;
+}
+
+.fa-reddit {
+  background: #ff5700;
+  color: white;
+}
+
+.fa-rss {
+  background: #ff6600;
+  color: white;
+}
+</style>
+</head>
+<body>
+<footer>
+    <br>
+<h2>Social Media</h2>
+<br>
+<nav class="socialnav">
+<div class="social-menu">
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="#" class="fa fa-linkedin"></a>
+<a href="#" class="fa fa-youtube"></a>
+</footer>
+</div>
+</nav>
+    
+</body> 
+<script src="./main_nav.js"></script> 
+
+</html> 
